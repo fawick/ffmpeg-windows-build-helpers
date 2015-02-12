4f8fa02256f2210d03a75d08b307bce2b4e64d1f
Fabian Wickborn <fabian.wickborn@diamond-air.at>
2015-02-12 20:27:41 +0100

    Add C99 compatible inttypes.h for MSVC 2010

--- mybuild.sh
+++ mybuild.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-./cross_compile_ffmpeg.sh --build-choice=win32 --build-ffmpeg-shared=y --build-ffmpeg-static=n
+./cross_compile_ffmpeg.sh \
+	--disable-nonfree=y \
+	--build-choice=win32 \
+	--build-ffmpeg-shared=y \
+	--build-ffmpeg-static=n
+OLDWD=$(pwd)
+cd sandbox/win32/ffmpeg_git_shared.installed/include
+if [ ! -f inttypes.h ] ; then
+	curl -O https://msinttypes.googlecode.com/svn/trunk/inttypes.h
+fi
+cd $OLDWD
+
